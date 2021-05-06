7b75468529929bb6f21013054f74d23a1ed45df7
joecole1011 <akawit_7@hotmail.com>
2021-05-06 22:43:20 +0700

    blog-detail
    blog-detail facebook comment v.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+(function (d, s, id) {
+    var fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    const js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v10.0';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $( window ).on('scroll', function(){
     var scrollTop = $(this).scrollTop()
     var score = '15px'
@@ -5,4 +13,24 @@ $( window ).on('scroll', function(){
         score = '5px 15px'
     }
     $('#navbar').css('padding', score)
+});
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav:false,
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:2,
+                
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    });
 });
